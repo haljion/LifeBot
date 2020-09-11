d93f47bf8d2bfd3a2fd99e496c29aa8960f48280
haljion <haljion.helmold@gmail.com>
2020-09-11 12:25:48 +0900

    軽微な修正

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 # botアカウントのトークンを指定
-API_TOKEN = "xoxb-1207637800480-1350577916593-GSbypP0NYEXgvFCuE9bT4l6y"
+API_TOKEN = "xoxb-1207637800480-1350577916593-Xnc8zp562Y20ZoE9681v0BTD"
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
-DEFAULT_REPLY = "おなかすいた"
+DEFAULT_REPLY = "デフォルト"
 
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
 PLUGINS = ["plugins"]
