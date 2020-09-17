21b244d3bfd2ec2e60b19d562134d983c33beb1c
haljion <haljion.helmold@gmail.com>
2020-09-17 17:57:35 +0900

    heroku

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -2,7 +2,7 @@
 import os
 
 # botアカウントのトークンはheroku上で指定
-API_TOKEN = os.environ["API_TOKEN"]
+API_TOKEN = os.environ.get("API_TOKEN")
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "「おしえて」って投稿すると使い方を表示するよ"
