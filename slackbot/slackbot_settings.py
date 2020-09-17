d7857ba445709c8812a5b6f400720fe6b50377bc
haljion <haljion.helmold@gmail.com>
2020-09-17 18:53:41 +0900

    heroku

--- slackbot/slackbot_settings.py
+++ slackbot/slackbot_settings.py
@@ -4,12 +4,10 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
-print(dotenv_path)
 load_dotenv(dotenv_path)
 
 # botアカウントのトークンを指定
 API_TOKEN = os.environ.get("API_TOKEN")
-print(API_TOKEN)
 
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "「おしえて」って投稿すると使い方を表示するよ"
